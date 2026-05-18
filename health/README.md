3cd6c5696532da783ac1f8e3d96ee319a63da814
Arthur-IBS <Arthur.Figrous@ibsplc.com>
2026-05-18 11:53:58 +0530

    Deleted all files for alfred to add new code

--- health/README.md
+++
@@ -1,2 +0,0 @@
-# IBS_project
-# this program is a demo program to test if the logs are being generated and to show that the program is running using postman
--- health/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('health/', views.HealthCheckView.as_view(), name='health_check'),
-]
